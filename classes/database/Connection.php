ed9444a6c58e87fcd72c5f3c775398d1d313eded
a.formella <a.formella@tme3c.com>
2018-11-27 13:16:07 +0100

    Drop support for PG < 8.0

--- classes/database/Connection.php
+++ classes/database/Connection.php
@@ -83,24 +83,23 @@ class Connection {
         }    
 
 		switch (substr($version,0,3)) {
-			case '10': return 'Postgres'; break;
-			case '10b': return 'Postgres'; break;
-			case '9.6': return 'Postgres'; break;
-			case '9.5': return 'Postgres'; break;
-			case '9.4': return 'Postgres94'; break;
-			case '9.3': return 'Postgres93'; break;
-			case '9.2': return 'Postgres92'; break;
-			case '9.1': return 'Postgres91'; break;
-			case '9.0': return 'Postgres90'; break;
-			case '8.4': return 'Postgres84'; break;
-			case '8.3': return 'Postgres83'; break;
-			case '8.2': return 'Postgres82'; break;
-			case '8.1': return 'Postgres81'; break;
-			case '8.0':
-			case '7.5': return 'Postgres80'; break;
-			case '7.4': return 'Postgres74'; break;
-			default: return 'Postgres'; break;
-		}
+      case '12b': return 'Postgres'; break;
+      case '11b': return 'Postgres11'; break;
+      case '10b': return 'Postgres10'; break;
+      case '9.6': return 'Postgres96'; break;
+      case '9.5': return 'Postgres95'; break;
+      case '9.4': return 'Postgres94'; break;
+      case '9.3': return 'Postgres93'; break;
+      case '9.2': return 'Postgres92'; break;
+      case '9.1': return 'Postgres91'; break;
+      case '9.0': return 'Postgres90'; break;
+      case '8.4': return 'Postgres84'; break;
+      case '8.3': return 'Postgres83'; break;
+      case '8.2': return 'Postgres82'; break;
+      case '8.1': return 'Postgres81'; break;
+      case '8.0': return 'Postgres80'; break;
+      default: return 'Postgres'; break;
+    }
 
 	    /* All <7.4 versions are not supported */
 	    // if major version is 7 or less and wasn't cought in the
